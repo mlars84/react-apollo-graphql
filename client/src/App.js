521c6c07e98fa2e7e3ee21fcbd802ee50ba52268
Matt Larson <matt.a.larson@gmail.com>
2017-10-13 16:29:50 -0500

    styling added

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const ChannelsList = () =>
+    (<ul className="Item-list">
+      <li>Channel 1</li>
+      <li>Channel 2</li>
+    </ul>)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>Welcome to Apollo</h2>
+        </div>
+        <ChannelsList />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
